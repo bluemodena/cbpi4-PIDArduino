265b93ff674c192415731c529432ee997b534121
bluemodena <bluemodena@gmail.com>
2022-01-08 10:08:04 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,21 +8,31 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 	long_description = f.read()
 
 setup(name='cbpi4-PIDArduino',
-      version='0.0.7',
-      description='CraftBeerPi4 PID Kettle Control Plugin',
+      version='0.0.9',
+      description='CraftBeerPi4 PID Arduino Kettle Control Plugin',
       author='David Kelly',
       author_email='bluemodena@gmail.com',
       url='https://github.com/bluemodena/cbpi4-PIDArduino',
       license='GPLv3',
       include_package_data=True,
       package_data={
-	      # If any package contains *.txt or *.rst files, include them:
-	      '': ['*.txt', '*.rst', '*.yaml'],
-	      'cbpi4-PIDArduino': ['*', '*.txt', '*.rst', '*.yaml']},
+        # If any package contains *.txt or *.rst files, include them:
+#      '': ['*.yaml'],
+      '': ['*.txt', '*.rst', '*.yaml'],
+#      'cbpi4-PIDArduino': ['*.yaml']},
+      'cbpi4-PIDArduino': ['*', '*.txt', '*.rst', '*.yaml']},
       packages=['cbpi4-PIDArduino'],
-      install_requires=[
-	      'cbpi>=4.0.0.52',
-      ],
       long_description=long_description,
       long_description_content_type='text/markdown'
       )
+#      package_data={
+#	      # If any package contains *.txt or *.rst files, include them:
+#	      '': ['*.txt', '*.rst', '*.yaml'],
+#	      'cbpi4-PIDArduino': ['*', '*.txt', '*.rst', '*.yaml']},
+#      packages=['cbpi4-PIDArduino'],
+#      install_requires=[
+#	      'cbpi>=4.0.0.52',
+#      ],
+#      long_description=long_description,
+#      long_description_content_type='text/markdown'
+#      )
